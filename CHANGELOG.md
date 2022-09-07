c7a03a1d39a5d91ba193ad7bf073fa6fb949500d
TfT Hacker <blockreference.com>
2022-09-07 21:14:00 +0200

    1.0.13 Fix to first day week setting not svaing

--- CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-# 1.0.11
-Fixes:
-- Natural Language Date form - was not formatting date properly if it was a 1 digit date (ex: oct 3, but working if oct 13). Reported in: https://github.com/TfTHacker/obsidian42-jump-to-date/issues/9 (Thank you @boksir)
-- Changed wording in Settings to better indicate that the jump-to-date calendar is being enabled/disabled for ribbon suipport.
-- Fixed an issue that the popup calendar still showed as an option in the command paleete if disabled in settings.
--- src/ui/settings.ts
+++ src/ui/settings.ts
@@ -51,16 +51,17 @@ export class SettingsTab extends PluginSettingTab {
 					await this.plugin.saveSettings();
 				});
 			});
-
-		new Setting(containerEl)
+			
+			new Setting(containerEl)
 			.setName('First day of the week on calendar is Sunday')
 			.setDesc('If toggled on the calendar will show the week starting with Sunday. If toggled off, it will show Monday as the starting day.')
 			.addToggle((cb: ToggleComponent) => {
 				cb.setValue(this.plugin.settings.firstDayOfWeekIsSunday);
 				cb.onChange(async (value: boolean) => {
 					this.plugin.settings.firstDayOfWeekIsSunday = value;
+					await this.plugin.saveSettings();
 				});
 			});
-
+ 
 	}
 }
